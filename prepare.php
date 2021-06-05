97ad98065562ddce4c995dd80eaf95e3679e399f
Herzu <dawidherc@gmail.com>
2021-06-05 16:08:49 +0200

    small cosmetic change

--- prepare.php
+++ prepare.php
@@ -43,10 +43,10 @@ if($db!=null)
 		$url = json_encode($item["StringFields"]["URL"]);
 		file_put_contents("connect.php",
 		"<?php".PHP_EOL .
-		"\$host = ".$url.PHP_EOL .";".
-		"\$db_user = ".$username.PHP_EOL .";".
-		"\$db_password = ".$password.PHP_EOL .";".
-		"\$db_name = ".$title.PHP_EOL .";".
+		"\$host = ".$url.";".PHP_EOL .
+		"\$db_user = ".";".$username.PHP_EOL .
+		"\$db_password = ".";".$password.PHP_EOL .
+		"\$db_name = ".$title.";".PHP_EOL .
 		"?>");
 	}
 	echo "Prepared db credentials successfully".PHP_EOL;
