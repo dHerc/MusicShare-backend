ac8b4207206d5a5ac90b26aa1e3dac82d958d58c
dHerc <43823377+dHerc@users.noreply.github.com>
2021-06-05 23:14:25 +0200

    variable names fix

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+$host = $url["host"];
+$db_user = $url["user"];
+$db_password = $url["pass"];
+$db_name = substr($url["path"], 1);
 ?>
