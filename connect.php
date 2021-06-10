b4e6dbe1956f977fca9aa07dc25aec7bf876c970
dHerc <43823377+dHerc@users.noreply.github.com>
2021-06-10 22:23:39 +0200

    enviromental vars fix

--- connect.php
+++ connect.php
@@ -1,6 +1,8 @@
 <?php
-$host = "eu-cdbr-west-01.cleardb.com";
-$db_user = "b54c9fb19f86b3";
-$db_password = "7b809f8b";
-$db_name = "heroku_1da3179c182a343";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$db_user = $url["user"];
+$db_password = $url["pass"];
+$db_name = substr($url["path"], 1);
 ?>
